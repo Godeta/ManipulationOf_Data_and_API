4e7b1ee7c10a0a9d279f1098856d0bb65f2663dc
Godeta <Godeta61@gmail.com>
2020-04-18 22:45:02 +0200

    Ajout de deux courbes au graphique

--- CSV_graphic/traitement.js
+++ CSV_graphic/traitement.js
@@ -6,14 +6,31 @@
           type: 'line', //type de graphe
           data: {
               labels: data.xlabels, //nom
-              datasets: [{
-                  label: 'Combined Land-Surface Air and Sea-Surface Water Temperature Anomalies in C°', //titre
-                  data: data.ytemps, //données
-                  fill: false, //ne pas remplir la courbe
-                  backgroundColor: 'rgba(255, 99, 132, 0.2)', //couleurs et affichage
-                  borderColor: 'rgba(255, 99, 132, 1)',
-                  borderWidth: 1
-              }]
+              datasets: [{ //les différentes courbes
+                      label: 'Global temperature in C°', //titre
+                      data: data.ytemps, //données
+                      fill: false, //ne pas remplir la courbe
+                      backgroundColor: 'rgba(255, 99, 132, 0.2)', //couleurs et affichage
+                      borderColor: 'rgba(255, 99, 132, 1)',
+                      borderWidth: 1
+                  },
+                  {
+                      label: 'Northern Hemisphere Temperature in °C',
+                      data: data.northern, //autre données
+                      fill: false,
+                      borderColor: 'rgba(99, 132, 255, 1)',
+                      backgroundColor: 'rgba(99, 132, 255, 0.5)',
+                      borderWidth: 1
+                  },
+                  {
+                      label: 'Souther Hemisphere in °C',
+                      data: data.southern,
+                      fill: false,
+                      borderColor: 'rgba(99, 255, 132, 1)',
+                      backgroundColor: 'rgba(99, 255, 132, 0.5)',
+                      borderWidth: 1
+                  }
+              ]
           },
           options: {
               scales: {
@@ -38,26 +55,35 @@
   //récupère les données
   async function getData() {
 
+      //degrés à gauche
       const xlabels = [];
+      //différentes valeurs en ordonné
       const ytemps = [];
+      const northern = [];
+      const southern = [];
 
       const response = await fetch("ZonAnn.Ts+dSST.csv");
       const data = await response.text();
 
       //séparer les données manuellement
-      const table = data.split("\n").slice(1); //chaqu e ligne puis on enlève la 2ème ligne
+      const table = data.split("\n").slice(1); //chaque ligne puis on enlève la 2ème ligne
       table.forEach(row => {
           const column = row.split(","); //on sépare chaque case
           const year = column[0]; //les années
-          xlabels.push(year);
-          const temp = column[1]; //1er temperature
+          xlabels.push(year); //les années en abscisse
+          const temp = column[1]; //la température globale
+
+          //affichage différence par rapport à la température moyenne de 14 degrés
           ytemps.push(parseFloat(temp) + 14);
-          //car affichage différence par rapport à la température moyenne de 14 degrés
+          northern.push(14 + parseFloat(column[2]));
+          southern.push(14 + parseFloat(column[3]));
           console.log(year, temp);
       });
       //console.log(table);
       return {
-          xlabels,
-          ytemps
+          xlabels, //années
+          ytemps, //températures °C
+          northern, //autres températures
+          southern
       };
   }
